9b62ad23c49cfe6b98d7c74a0cd2101af068fd42
Dakotah Haughey <142916881+dhaughey72@users.noreply.github.com>
2024-11-07 20:27:12 -0600

    nav

--- java/script.js
+++ java/script.js
@@ -2,3 +2,13 @@
 function popUp() {
     document.getElementById("banner").style.display = "none";
 }
+
+function mobnav() {
+    var x = document.getElementById("mob-nav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } 
+    else {
+        x.style.display = "block";
+    }
+}
